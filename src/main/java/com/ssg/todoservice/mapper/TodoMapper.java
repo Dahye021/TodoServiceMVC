8bf6e856a35c12c4b108ba5aca0e4cc43aea7593
Dahye <hnu4051@naver.com>
2025-11-01 23:54:01 +0900

    home

--- src/main/java/com/ssg/todoservice/mapper/TodoMapper.java
+++ src/main/java/com/ssg/todoservice/mapper/TodoMapper.java
@@ -1,6 +1,7 @@
 package com.ssg.todoservice.mapper;
 
 import com.ssg.todoservice.domain.TodoVO;
+import com.ssg.todoservice.dto.PageRequestDTO;
 
 import java.util.List;
 
@@ -11,4 +12,6 @@ public interface TodoMapper {
     TodoVO selectOne(Long tno);
     void  delete(Long tno);
     void update(TodoVO todoVO);
+    List<TodoVO> selectList(PageRequestDTO pageRequestDTO);
+    int getCount(PageRequestDTO pageRequestDTO);
 }
